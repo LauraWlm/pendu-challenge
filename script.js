589a46c80b11a662e2f0010a156d7e3c9bffd2a9
Laura Wilhelmi <laurawilhelmi.pro@gmail.com>
2023-03-07 16:21:05 +0100

    all

---
+++ script.js
@@ -0,0 +1,10 @@
+//chaine de caractère pouvant être utilisées
+let alphabet = 'abcdefghijklmnopqrstuvwxyz';
+let mot = "taxidermie";
+let rev = mot.split('')
+let rev2 = mot.replace(/[a-z]/g, '_ ');
+console.log(rev);
+console.log(rev2);
+
+
+
